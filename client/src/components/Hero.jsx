f7faf625c003890bba534a8066683573ddbd92a0
Anikesh Kumar <anikesh6203@gmail.com>
2025-08-19 19:04:03 +0530

    hero update

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { cities, assets } from "../assets/assets";
 const Hero = () => {
   return (
-    <div className="flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:32 text-white bg-[url('/src/assets/heroImage.png')] bg-no-repeat bg-cover bg-center h-screen ">
+    <div className="flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:32 text-white bg-[url('/src/assets/heroImg.png')] bg-no-repeat bg-cover bg-center opacity-93 h-screen ">
       <p className="bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20 ">
         The Ultimate Hotel Experience
       </p>
