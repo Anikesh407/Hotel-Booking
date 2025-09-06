c0f1a7d83db49b9c9faaaa4e14d4023810ac5f97
Anikesh Kumar <anikesh6203@gmail.com>
2025-09-06 17:27:00 +0530

    import fixing

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import FeatureDestination from "../components/FeatureDestination";
 import ExclusiveOffers from "../components/ExclusiveOffers";
-import Testimonial from "../components/testimonial";
+import Testimonial from "../components/Testimonial";
 import NewsLetter from "../components/NewsLetter";
 import RecommendedHotels from "../components/RecommendedHotels";
 const Home = () => {
