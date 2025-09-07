85a9b09f9bb5e6eddeb1d316ef3cebf1d229d346
Anikesh Kumar <anikesh6203@gmail.com>
2025-09-08 00:06:41 +0530

    fix dashboard fetch data at dashboard commponent

--- client/src/Pages/hotelOwner/Dashboard.jsx
+++ client/src/Pages/hotelOwner/Dashboard.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from "react";
 import Title from "../../components/Title";
 import { assets } from "../../assets/assets";
 import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const Dashboard = () => {
-  const { currency, user, getToken, toast, axios } = useAppContext();
+  const { currency, user, getToken, axios } = useAppContext();
 
   const [dashboardData, setDashboardData] = useState({
     bookings: [],
@@ -14,7 +15,7 @@ const Dashboard = () => {
 
   const fetchDashboardData = async () => {
     try {
-      const data = await axios.get("/api/bookings/hotel", {
+      const { data } = await axios.get("/api/bookings/hotel", {
         headers: { Authorization: `Bearer ${await getToken()}` },
       });
 
