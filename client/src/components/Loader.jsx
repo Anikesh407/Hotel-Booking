5d94f4567d03a1b136488aab2eb6a063b456d53a
Anikesh Kumar <anikesh6203@gmail.com>
2025-09-09 20:13:11 +0530

    fix loader size

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -14,7 +14,7 @@ const Loader = () => {
   }, [nextUrl]);
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-primary"></div>
+      <div className="animate-spin rounded-full h-16 w-16 border-4 border-gray-300 border-t-gray-600"></div>
     </div>
   );
 };
